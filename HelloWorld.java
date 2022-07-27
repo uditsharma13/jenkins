6b5d647e02773bd6d4ef598aea4b4b5740172658
Ram Niwas Sangwan <ramnsangwan@gmail.com>
2022-07-27 10:43:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World from Oracle DevOps create on Jun 29, 2022");
+    System.out.println("Hello, World from Oracle DevOps create on July 27, 2022");
   }
 }
